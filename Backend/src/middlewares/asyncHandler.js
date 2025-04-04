74c5e471243956118a826d667d3e25924e2c7124
Minh20812 <blossworld12@gmail.com>
2025-04-04 21:17:56 +0700

    update asyncHandler

--- Backend/src/middlewares/asyncHandler.js
+++ Backend/src/middlewares/asyncHandler.js
@@ -1,6 +1,14 @@
+// const asyncHandler = (fn) => (req, res, next) => {
+//   Promise.resolve(fn(req, res, next)).catch((error) => {
+//     res.status(500).json({ message: error.message });
+//   });
+// };
+
+// export default asyncHandler;
+
 const asyncHandler = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch((error) => {
-    res.status(500).json({ message: error.message });
+    next(error); // Chuyển lỗi cho middleware xử lý lỗi tiếp theo
   });
 };
 
