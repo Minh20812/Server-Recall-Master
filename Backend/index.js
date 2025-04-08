84702194bd0110a26c5f59b09fa1db4a078a5836
Minh20812 <blossworld12@gmail.com>
2025-04-08 13:04:12 +0700

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -18,8 +18,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    // origin: ["https://recallmaster.vercel.app", "http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://recallmaster.vercel.app", "http://localhost:5173"],
     credentials: true,
     exposedHeaders: ["Set-Cookie"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
